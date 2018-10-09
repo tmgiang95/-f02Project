1b5f202965e31233b9d5ac3403a96012ebb27bb9
TUNG <superyasuo2803@gmail.com>
2018-10-09 19:27:44 +0700

    tung init

--- ProjectF02/ProjectF02/Modules/LoginScreen/ViewController.swift
+++ ProjectF02/ProjectF02/Modules/LoginScreen/ViewController.swift
@@ -75,10 +75,6 @@ class ViewController: BaseViewController, GIDSignInUIDelegate {
         }
     }
     
-    func getUserFromFireBase() -> User {
-        var user: User
-        
-    }
     
     @IBAction func signupBtnAction(_ sender: Any) {
         let registerVC = RegisterViewController()
