32458d3e19706daf9d5701fe7d622c6aa18b9e25
tung2803 <superyasuo2803@gmail.com>
2018-10-25 20:40:31 +0700

    show profile

--- ProjectF02/ProjectF02/Modules/MyProfileVC/MyProfileViewController.swift
+++ ProjectF02/ProjectF02/Modules/MyProfileVC/MyProfileViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 import FirebaseDatabase
 
 class MyProfileViewController: UIViewController {
-    
+    var userinfo: User!
+    var posts = [Post]()
     var ref : DatabaseReference?
     var databaseHandle: DatabaseHandle?
     @IBOutlet weak var ProfileTableview: UITableView!
@@ -21,10 +22,15 @@ class MyProfileViewController: UIViewController {
     
     }
     
+    func fillData(_ user: User)
+    {
+     userinfo = user
+    }
+    
     func configureTableview() {
         ProfileTableview.dataSource = self
         ProfileTableview.delegate = self
-//        ProfileTableview.registerCells(AvatarTableViewCell.className,PostTableViewCell.className)
+        ProfileTableview.registerCell(AvatarTableViewCell.className,PostTableViewCell.className)
     }
     
 }
@@ -35,20 +41,28 @@ extension MyProfileViewController: UITableViewDelegate {
     }
 }
 extension MyProfileViewController: UITableViewDataSource {
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return userinfo.count
-//    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if section == 0 {
+            return 1
+        }
+        else {
+            return posts.count
+        }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
+        if indexPath.section == 0 {
             guard let cell = ProfileTableview.dequeueReusableCell(withIdentifier: AvatarTableViewCell.className, for: indexPath) as? AvatarTableViewCell else {
                 return UITableViewCell()
             }
-            
+            let username = userinfo.lastName! + " " + userinfo.firstName!
+            let coverimage: UIImage = #imageLiteral(resourceName: "cover")
+            let avatarimage: UIImage = #imageLiteral(resourceName: "avatar")
+            cell.fillData(coverimage , avatarimage, username , avatarimage)
             return cell
         }
         else {
